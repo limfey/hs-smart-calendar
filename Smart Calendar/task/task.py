851c6ee21b00b6835efee6cdaa904cd2cb7fc7f1
Dmitrii Zozulenko <dmitriizozulenko@MacBook-Pro-Dmitrii.local>
2023-07-16 01:54:57 +0300

    win

--- Smart Calendar/task/task.py
+++ Smart Calendar/task/task.py
@@ -1,4 +1,22 @@
 import datetime
-print('Current date and time:')
+
+
+def timedelta_convert(duration):
+    days, seconds = duration.days, duration.seconds
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    return days, hours, minutes
+
+
 date_time = datetime.datetime.now()
-print(date_time)
+print(f"Current date and time:\n{date_time}")
+print("Let's add a note to the calendar.")
+year = input("Enter year:\n")
+month = input("Enter month:\n")
+day = input("Enter day:\n")
+hour = input("Enter hour:\n")
+minute = input("Enter minute:\n")
+text = input("Enter a text:\n")
+delta = datetime.datetime(int(year), int(month), int(day), int(hour), int(minute)) - date_time
+days, hours, minutes = timedelta_convert(delta)
+print(f'Before the event note "{text}" remains:\n{days} day(s), {hours} hour(s) {minutes} minute(s)')
