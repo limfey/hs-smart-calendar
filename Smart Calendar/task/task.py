bc9d213080d0ebd2324ad579fca3a397aaabc0ab
Dmitrii Zozulenko <dmitriizozulenko@MacBook-Pro-Dmitrii.local>
2023-07-15 17:00:11 +0300

    sd

--- Smart Calendar/task/task.py
+++ Smart Calendar/task/task.py
@@ -2,9 +2,3 @@ import datetime
 print('Current date and time:')
 date_time = datetime.datetime.now()
 print(date_time)
-df
-fewvrvrf
-dvcdvr
-dfgdf
-
-execfdfdfdvevrvrgit remote set-url origin git@github.com:OWNER/REPOSITORY.gitevdfdf
